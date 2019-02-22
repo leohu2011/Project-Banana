ca2c28b438024f540110136e8c68bbf3badb7156
Hu Qi <qhu@bbmtek.com>
2019-02-21 22:38:42 -0500

    Initial Commit

---
+++ AutoChessHelper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoChessHelper
+//
+//  Created by HU QI on 2019-02-21.
+//  Copyright © 2019 HU QI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
